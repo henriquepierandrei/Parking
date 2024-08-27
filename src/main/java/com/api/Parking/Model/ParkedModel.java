f723925ce9eb729836e82024bb1494e4d761a7f5
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-27 13:55:59 -0300

    adicionando a variavel date

--- src/main/java/com/api/Parking/Model/ParkedModel.java
+++ src/main/java/com/api/Parking/Model/ParkedModel.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -23,6 +24,8 @@ public class ParkedModel {
     @ManyToOne
     private CarModel car;
 
+
+    private LocalDate date;
 
     private LocalDateTime dateTimeArrival;
 
